eaef83bbd9677f7771aa91c12fb1bcf605aa6747
Iagoccarvalho <iagocarvalho_@outlook.com>
2024-04-21 10:30:49 -0300

    feat: Navegação de páginas com Bottom-Tabs

---
+++ pages/passwords/index.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function Passwords(){
+    return(
+        <Text>Minhas senhas</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
